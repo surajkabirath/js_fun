f6010d37f99c826df6704cd6a3dca630d3df40d6
surajkabirath <surajkabirath1234@gmail.com>
2024-07-29 23:02:30 +0545

    day-1 final stage

--- day1/script.js
+++ day1/script.js
@@ -1,16 +1,17 @@
-document.querySelectorAll(".question").forEach((question) => {
-  question.addEventListener("click", () => {
-    const answer = question.nextElementSibling;
-    const icon = question.querySelector(".icon");
+document.querySelectorAll('.question').forEach(question => {
+    question.addEventListener('click', () => {
+        const answer = question.nextElementSibling;
+        const icon = question.querySelector('.icon');
 
-    if (answer.classList.contains("show")) {
-      answer.classList.remove("show");
-      icon.innerHTML = '<path d="M12 19l-7-7h14l-7 7z" fill="currentColor"/>';
-    }else{
-        document.querySelectorAll(".answer").forEach(e=>e.classList.remove("show"));
-        document.querySelectorAll(".icon").forEach(i => i.innerHTML='<path d="M12 19l-7-7h14l-7 7z" fill="currentColor"/>');
-        answer.classList.add("show");
-        icon.innerHTML = '<path d="M12 5l-7 7h14l-7-7z" fill="currentColor"/>';
-    }
-  });
+        if (answer.classList.contains('show')) {
+            answer.classList.remove('show');
+            icon.innerHTML = '<path d="M12 19l-7-7h14l-7 7z" fill="currentColor"/>';
+        } else {
+            document.querySelectorAll('.answer').forEach(a => a.classList.remove('show'));
+            document.querySelectorAll('.icon').forEach(i => i.innerHTML = '<path d="M12 19l-7-7h14l-7 7z" fill="currentColor"/>');
+
+            answer.classList.add('show');
+            icon.innerHTML = '<path d="M12 5l-7 7h14l-7-7z" fill="currentColor"/>';
+        }
+    });
 });
