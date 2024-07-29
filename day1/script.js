55a7ac92673494132ebb5d681ae6f5f1d262ca6f
surajkabirath <surajkabirath1234@gmail.com>
2024-07-29 23:20:38 +0545

    pre final

--- day1/script.js
+++ day1/script.js
@@ -1,11 +1,16 @@
+
+
 document.querySelectorAll('.question').forEach(question => {
     question.addEventListener('click', () => {
         const answer = question.nextElementSibling;
+       
+        
         const icon = question.querySelector('.icon');
 
         if (answer.classList.contains('show')) {
             answer.classList.remove('show');
             icon.innerHTML = '<path d="M12 19l-7-7h14l-7 7z" fill="currentColor"/>';
+            console.log("close")
         } else {
             document.querySelectorAll('.answer').forEach(a => a.classList.remove('show'));
             document.querySelectorAll('.icon').forEach(i => i.innerHTML = '<path d="M12 19l-7-7h14l-7 7z" fill="currentColor"/>');
