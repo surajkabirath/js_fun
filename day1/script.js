df62f0fe15e548f5001c5c6a15ced1bfb1815ad6
surajkabirath <surajkabirath1234@gmail.com>
2024-07-29 22:48:38 +0545

    js

--- day1/script.js
+++ day1/script.js
@@ -8,7 +8,9 @@ document.querySelectorAll(".question").forEach((question) => {
       icon.innerHTML = '<path d="M12 19l-7-7h14l-7 7z" fill="currentColor"/>';
     }else{
         document.querySelectorAll(".answer").forEach(e=>e.classList.remove("show"));
-        document.querySelectorAll(".icon").forEach(i => i.innerHTML='<path d="M12 19l-7-7h14l-7 7z" fill="currentColor"/>')
+        document.querySelectorAll(".icon").forEach(i => i.innerHTML='<path d="M12 19l-7-7h14l-7 7z" fill="currentColor"/>');
+        answer.classList.add("show");
+        icon.innerHTML = '<path d="M12 5l-7 7h14l-7-7z" fill="currentColor"/>';
     }
   });
 });
