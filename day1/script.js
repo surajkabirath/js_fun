d883a15dc0795b20c581df660b81430645afdddf
surajkabirath <surajkabirath1234@gmail.com>
2024-07-29 22:17:41 +0545

    day 1

---
+++ day1/script.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('.question').forEach(question => {
+    question.addEventListener('click', () => {
+        const answerId = question.getAttribute('data-id');
+        const answer = document.getElementById(`answer${answerId}`);
+        const icon = question.querySelector('.icon');
+
+        if (answer.classList.contains('show')) {
+            answer.classList.remove('show');
+            icon.innerHTML = '<path d="M12 19l-7-7h14l-7 7z" fill="currentColor"/>';
+        } else {
+            document.querySelectorAll('.answer').forEach(a => a.classList.remove('show'));
+            document.querySelectorAll('.icon').forEach(i => i.innerHTML = '<path d="M12 19l-7-7h14l-7 7z" fill="currentColor"/>');
+
+            answer.classList.add('show');
+            icon.innerHTML = '<path d="M12 5l-7 7h14l-7-7z" fill="currentColor"/>';
+        }
+    });
+});
