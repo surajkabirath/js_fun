cd7780b0ccf25e7733e1f0b5d3eb140a7278b2cf
surajkabirath <surajkabirath1234@gmail.com>
2024-07-31 09:01:40 +0545

    day2

--- day2-Product_Design_Cart/script.js
+++ day2-Product_Design_Cart/script.js
@@ -1,5 +1,5 @@
 const descriptionContains = document.querySelector(".product-description");
-const maxDescriptionLength = 83;
+const maxDescriptionLength = 150;
 if (descriptionContains.textContent.length > maxDescriptionLength) {
   descriptionContains.textContent =
     descriptionContains.textContent.slice(0, maxDescriptionLength) + "...";
