34c8d0e2f02d07f48804e06a6b493bdda859971b
surajkabirath <surajkabirath1234@gmail.com>
2024-08-01 21:56:30 +0545

    cc

---
+++ day4-toast_notification/script.js
@@ -0,0 +1,27 @@
+function showToast(message, type) {
+    const toastContainer = document.getElementById('toast-container');
+    const toast = document.createElement('div');
+    toast.classList.add('toast', type);
+
+    toast.innerHTML = `
+        <span>${message}</span>
+        <button class="close-btn" onclick="closeToast(this)">✖</button>
+    `;
+
+    toastContainer.appendChild(toast);
+
+    setTimeout(() => {
+        toast.classList.add('show');
+    }, 100);
+
+    setTimeout(() => {
+        closeToast(toast);
+    }, 3000);
+}
+
+function closeToast(element) {
+    element.classList.remove('show');
+    setTimeout(() => {
+        element.remove();
+    }, 300);
+}
