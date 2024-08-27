2780226ed498459fd6cd9d4bd9f70b2964367dd6
surajkabirath <surajkabirath1234@gmail.com>
2024-08-27 20:06:10 +0545

    minor change

--- day7-kyc/script.js
+++ day7-kyc/script.js
@@ -6,6 +6,7 @@ document.getElementById('kycForm').addEventListener('submit', function(e) {
     const dob = formData.get('dob');
     const address = formData.get('address');
     const idType = formData.get('idType');
+    console.log(fullName)
     console.log(address)
     console.log(idType)
     
